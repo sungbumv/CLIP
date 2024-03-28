4970c58ad8f29d36426854ca87ffa25cc657778f
sungbumv <sungbumv@gmail.com>
2024-03-28 22:02:25 +0900

    feat : Create project - KDSB
    - KDSB : KDS + SB  KDS프로그램을 일단 만들어보기 위함

---
+++ KDSB/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace KDSB
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ KDSB/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KDSB
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
