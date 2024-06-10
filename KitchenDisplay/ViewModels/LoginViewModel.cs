f0b6b2ceee5f0db89fbf6737c8306cfc6a43a5d2
sungbumv <sungbumv@gmail.com>
2024-06-10 22:22:27 +0900

    feat : userModel 생성중
    - 로그인에 필요한 화면 개선 진행중

--- KitchenDisplay/ViewModels/LoginViewModel.cs
+++ KitchenDisplay/ViewModels/LoginViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KitchenDisplay.ViewModels.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,22 @@ using System.Threading.Tasks;
 
 namespace KitchenDisplay.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : CommonViewModel
     {
+        private string userid;
+        private string password;
+        private string statusMsg;
+
+        public string UserId
+        {
+            get => userid;
+            set
+            {
+                userid = value;
+                OnPropertyChanged();
+            }
+        }
+
+
     }
 }
