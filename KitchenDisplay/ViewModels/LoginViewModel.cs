af654d7679e1fca6cb7cab62f7035ecc7af8aff2
sungbumv <sungbumv@gmail.com>
2024-06-10 22:26:42 +0900

    feat : 로그인 뷰모델 환경 정리
    - LoginView 화면 추가

--- KitchenDisplay/ViewModels/LoginViewModel.cs
+++ KitchenDisplay/ViewModels/LoginViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using KitchenDisplay.ViewModels.Common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace KitchenDisplay.ViewModels
 {
@@ -23,6 +26,60 @@ namespace KitchenDisplay.ViewModels
             }
         }
 
+        public string Password
+        {
+            get => password;
+            set
+            {
+                password = value;
+                OnPropertyChanged();
+            }
+        }
 
+        public string StatusMsg
+        {
+            get => statusMsg;
+            set
+            {
+                statusMsg = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new RelayCommand(Login, CanLogin);
+        }
+
+        private void Login(object parameter)
+        {
+            if (AuthenticateUser(Username, Password))
+            {
+                StatusMessage = "Login successful!";
+            }
+            else
+            {
+                StatusMessage = "Invalid username or password.";
+            }
+        }
+
+        private bool CanLogin(object parameter)
+        {
+            return !string.IsNullOrEmpty(UserId) && !string.IsNullOrEmpty(Password);
+        }
+
+        private bool AuthenticateUser(string userid, string password)
+        {
+            return userid == "admin" && password == "password";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
